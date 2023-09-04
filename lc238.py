4e3e08e9672fb5acc6c40f813346e0d2e7ea2038
JUB_Yoush <yoush1337@gmail.com>
2023-09-04 08:27:14 -0400

    o(1) memory solution for 238

--- lc238.py
+++ lc238.py
@@ -24,6 +24,18 @@ class Solution:
                 output[i] = prefix[i-1] * postfix[i+1]
         return output
 
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        res = [1] * len(nums)
+        prefix = 1
+        for i in range(len(nums)):
+            res[i] = prefix
+            prefix *= nums[i]
+        postfix = 1 
+        for i in range(len(nums) - 1,-1,-1):
+            res[i] *= postfix
+            postfix *= nums[i]
+        return res
+
 
             
 
