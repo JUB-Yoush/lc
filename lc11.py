08bf0436add2b5c39918eb198ac3406b9455445b
JUB_Yoush <yoush1337@gmail.com>
2023-09-10 14:17:25 -0400

    container w most water

--- lc11.py
+++ lc11.py
@@ -1,10 +1,16 @@
-class Solution:
-    def maxArea(self, height: List[int]) -> int:
-        area = 0
-        l = 0
-        r = len(height) -1
-        while l < r:
-            area = max(min(height[l],height[r]) * (r-l) ,area)
-            if height[l]<= height[r]: l += 1
-            elif height[r] < height[l]: r -= 1
-        return area 
+def maxArea(height: list[int]) -> int:
+    l = 0
+    r = len(height) -1
+    highest_area = 0
+    while r > l:
+        length = min(height[l],height[r])
+        width = r - l
+        area = length * width
+        highest_area = max(highest_area,area)
+        if height[l] > height[r]:
+            r-=1
+        elif height[l] <= height[r]:
+            l+=1
+    return highest_area
+            
+print(maxArea([1,8,100,2,100,4,8,3,7]))
