2833b131f155fea6cf04c96de1f926e51f234143
JUB_Yoush <yoush1337@gmail.com>
2023-09-12 08:32:07 -0400

    daily temperatures, NC solution

--- lc739.py
+++ lc739.py
@@ -1,3 +1,12 @@
 class Solution:
-    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+    def dailyTemperatures(self, temperatures: list[int]) -> list[int]:
+        output = [0] * len(temperatures)
+        stack = []
+        for i,t in enumerate(temperatures):
+            while stack and t >   stack[-1][0]: # is this temp greater then the top of the stack?
+                stackT, stackInd = stack.pop()
+                res[stackInd] = (i - stackInd) # return the # of days it took to find a greater temperature
+            stack.append([t,i]) # add the tempurature and index
+        return res
         
+
