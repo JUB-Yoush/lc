1e453bc3f9f0bf1f7845ada285903ea0ca18eddd
JUB_Yoush <yoush1337@gmail.com>
2023-09-20 19:37:14 -0400

    finish up 74

---
+++ lc74.py
@@ -0,0 +1,20 @@
+def searchMatrix(matrix: list[list[int]], target: int) -> bool:
+	# do binary search on leftmost column to find out which row to check 
+	max_row = len(matrix[0])-1
+	high = len(matrix) -1
+	low = 0
+	answer_row = []
+	while True:
+		center = len(matrix) -1 //2
+		if matrix[center][0] == target:
+			return True
+		if matrix[center][0] <
+		if matrix[center][0] > target:
+			center 
+		elif matrix[center][0] > target and matrix[center][max_row] < target:
+			answer_row = matrix[center]
+			
+
+
+
+print(searchMatrix([[1,3,5,7],[10,11,16,20],[23,30,34,60]],3))
