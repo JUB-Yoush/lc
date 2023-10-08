00840254355a90aed7db8c3f335edea7a96492dc
JUB_Yoush <yoush1337@gmail.com>
2023-10-08 18:11:55 -0400

    138

--- lc138.py
+++ lc138.py
@@ -1,31 +1,50 @@
 """
-# Definition for a Node.
+import llist
 class Node:
     def __init__(self, x: int, next: 'Node' = None, random: 'Node' = None):
         self.val = int(x)
         self.next = next
         self.random = random
+
+
+def copyRandomList(head):
+        oldlist = head
+        newlist = Node(head.val)
+        newhead = newlist
+        nodehash = {} #oldnode:newnode
+        nodehash[oldlist] = newlist
+        # populate hashmap
+        while oldlist!= None:
+            next = Node(oldlist.next.val)
+            newlist.next = next
+            newlist = next
+            nodehash[oldlist] = newlist
+            oldlist = oldlist.next
+       
+       # go again and assign rng ptrs 
+        oldlist = head
+        newlist = newhead
+        while oldlist != None:
+            newlist.random = nodehash[oldlist.random]
+            newlist = newlist.next
+            oldlist = oldlist.next
+            
 """
 
-class Solution:
-    def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
-        nodes= []
-        curr = head
-        nodeHash = {}
 
-        # make dc of each node, map to the hashmap
-        while curr:
-            newNode = Node(curr.val)
-            nodeHash[curr] = newNode
-            curr = curr.next
-        nodeHash[None] = None 
+def copyRandomList(head):
+    oldToCopy = {None:None}
+    curr = head
+    while curr:
+        copy = Node(cur.val)
+        oldToCopy[curr]
+        curr = curr.next
+    curr = head
+    while curr:
+        copy = oldToCopy[cur]
+        copy.next = oldToCopy[curr.next]
+        copy.random = oldToCopy[cur.random]
+        cur = cur.next
+    return oldToCopy[head]
 
-        for oldNode,newNode in nodeHash.items():
-            if oldNode != None:
-                newNode.next = nodeHash[oldNode.next]
-                newNode.random = nodeHash[oldNode.random]
-        
-        return nodeHash[head]
-        
-
-            
+    
