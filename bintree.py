addd6966b0f97a04c961d05c02b727d8256e0ce7
JUB_Yoush <yoush1337@gmail.com>
2023-10-31 18:35:01 -0400

    should make a bin tree library

---
+++ bintree.py
@@ -0,0 +1,12 @@
+class TreeNode:
+	def __init__(self, val=0, left=None,right=None):
+		self.val = val
+		self.right = right
+		self.left = left
+
+def make_tree(inputArr):
+	for input in inputArr:
+	pass
+
+	
+	
