2ef5ba34779f725cf8513cbae3a4afb60a75034c
JUB-Yoush <yoush1337@gmail.com>
2023-12-14 09:57:43 -0500

    backtracking got hands

--- lc39.py
+++ lc39.py
@@ -1,3 +1,17 @@
 #https://leetcode.com/problems/combination-sum/
+"""
+what is the thinking process for adding numbers?
+because the system otherwise is fairly simple:
+- pick a number + add to current sum
+- if current sum == target: check if combination already exists in hash
+- if dont exist then add
+- continute until every option exausted
+- so start with an empty list
+- make the decision to add [] or that number
+- but you can add numbers repeatedly so when do you... stop adding that number
+- [], current number, or move to next number
+- three branches instead of two?
+"""
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        pass
